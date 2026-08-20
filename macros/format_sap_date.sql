10187ef46bd2fb22ddc517c3459a377faf04e1dc
Asha Kesavulu <kesavaluasha@gmail.com>
2026-08-20 03:57:17 +0000

    Added few tests and macros

--- macros/format_sap_date.sql
+++ macros/format_sap_date.sql
@@ -1,7 +1,19 @@
-{% macro format_sap_date(column_name) %}
-    concat(
-        substr({{ column_name }}, 6, 2), '/',
-        substr({{ column_name }}, 9, 2), '/',
-        substr({{ column_name }}, 1, 4)
-    )
-{% endmacro %}
+{% macro format_sap_date(column_name) -%}
+
+    case
+        when {{ column_name }} is null then null
+        when trim(cast({{ column_name }} as string)) in (
+            '',
+            '00000000',
+            '0000-00-00'
+        ) then null
+        else concat(
+            substr({{ column_name }}, 6, 2),
+            '/',
+            substr({{ column_name }}, 9, 2),
+            '/',
+            substr({{ column_name }}, 1, 4)
+        )
+    end
+
+{%- endmacro %}
--- models/intermediate/e2e_sap_matr_dochdr.sql
+++ models/intermediate/e2e_sap_matr_dochdr.sql
@@ -1,17 +1,13 @@
 with
-    mkpf as (select * from {{ ref("mkpf") }}),
+    mkpf as (select * from {{- ref("mkpf") -}}),
     matr_dochdr as (
 
         select
             mblnr,
             mjahr,
             blart,
-            concat(
-                substr(bldat, 6, 2), '/', substr(bldat, 9, 2), '/', substr(bldat, 1, 4)
-            ) as bldat,
-            concat(
-                substr(budat, 6, 2), '/', substr(budat, 9, 2), '/', substr(budat, 1, 4)
-            ) as budat,
+            {{- format_sap_date('bldat') -}} as bldat,
+            {{- format_sap_date('budat') -}} as budat,
             source,
             region
         from mkpf
